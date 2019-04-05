264bea81ebafe85b05ee242698bba950ade6739f
Ti <tianyizhang1987@gmail.com>
2019-04-04 21:03:55 -0700

    -

--- LinkedList.js
+++ LinkedList.js
@@ -6,60 +6,88 @@ export default class LinkedList {
     this.tail = null;
   }
 
-  append(value){
+  append(value) {
     var node = new LinkedListNode(value);
     if (!this.head) {
       this.head = node;
       this.tail = node;
-      return this; 
-    } 
-    this.tail.next = node; 
+      return this;
+    }
+    this.tail.next = node;
     this.tail = node;
-    return this; 
+    return this;
   }
 
-  prepend(value){
+  prepend(value) {
     var node = new LinkedListNode(value);
     if (!this.head) {
-      this.tail = node; 
+      this.tail = node;
     }
-    node.next = this.head; 
-    this.head = node; 
-    return this; 
+    node.next = this.head;
+    this.head = node;
+    return this;
   }
 
   toString() {
     var current = this.head;
     var str = ''
-    while (current)  {
+    while (current) {
+      console.log("current in to string is", current)
       str = str + current.value;
-      current = current.next; 
-    } 
+      current = current.next;
+    }
     return str.split('').join(',');
   }
 
   delete(value) {
-    var current = this.head; 
-    var previous
-    var deleted
-    if (current) {
-      if (current.value === value) {
-        // if the node to be deleted is the head, return
-        if (current === this.head) {
-          this.head = current.next; 
-          // return current ; 
-        }
-        previous.next = current.next; 
-        // return current; 
-        deleted = true; 
-      }
-        previous = current; 
-        current = current.next; 
-      console.log("if statement")
-    } else {
-      console.log("deleted", deleted)
-      return deleted ? value : null; 
+
+    // iterate through list, go from head to tail
+    if (!this.head) {
+      return null; 
     }
+    let current = this.head;
+    let previous = null;
+
+    const nodeToDelete = new LinkedListNode(value)
+    let deleted = false;
+
+    while (current.next) {
+      // if current.value === value, delete current 
+      if (current.value === value) {
+        console.log("1")
+        deleted = true;
+        // current node is head: 
+        // make next node head 
+        if (this.head === current) {
+          console.log("2")
+          this.head = this.head.next
+        } else if (this.tail === current) {
+          console.log("3")
+          // currentnode is tail: 
+          // make prev node.next null, make prev node tail 
+          previous.next = null;
+          this.tail = previous;
+        } else {
+          console.log(4)
+          // if current node in mid of list:
+          // remove node from list
+          // the node before the deleted node - make its next the deletednode.next
+          console.log("CURRENT IS", current)
+          console.log("previous", previous)
+          console.log("next", current.next)
+          console.log("replacing previous.next with current.next:", previous.next, current.next);
+          previous.next = current.next; 
+
+          console.log("previous", previous)
+          console.log("next", current.next)
+
+          console.log(this.toString())
+        }
+      }
+      previous = current;
+      current = current.next;
+    }
+    return deleted ? nodeToDelete : null;
   }
 }
 
