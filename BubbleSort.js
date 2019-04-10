468fc82c4e0e012c14947745a10742d29ee3dcde
Ti <tianyizhang1987@gmail.com>
2019-04-09 21:37:00 -0700

    Adds bubble sort and selection sort

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,18 +1,18 @@
 export default class BubbleSort {
-  sort(arr){
-    // Goes over array again and again until all elements smaller than elements to their right 
-    let originalArr = [...arr];
-    let swapped = false; 
+  sort(arr) {
+    let swapped;
     for (let i = 0; i < arr.length; i ++) {
-      swapped = false
-      if (arr[i] > arr[i + 1]) {
-        swapped = false; 
-        let temp = arr[i];
-        arr[i + 1] = temp;
-        arr[i] = arr[i + 1]
-      } else {
-        swapped = true;
+      for (let j = 0; j < arr.length - i; j ++) {
+        if (arr[j] > arr[j + 1]){
+          swapped = false;
+          let temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr [j + 1] = temp;
+        } else {
+          swapped = true;
+        }
       }
     }
+    return arr
   }
 }
