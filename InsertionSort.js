61741f3c7d23af420590a023ae0df0b4dd95f12f
Ti <tianyizhang1987@gmail.com>
2019-04-11 21:20:48 -0700

    adds insertion sort

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,3 +1,15 @@
 export default class InsertionSort {
-
+  sort(arr){
+    const origArr = [...arr];
+    for (let i = 1; i < arr.length; i++) {
+      for (let j = i ; j > 0; j--) {
+        if ( arr[j] < arr[j - 1]){
+          [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+        }
+      }
+    }
+    console.log(arr)
+    return arr
+  } 
 }
+
